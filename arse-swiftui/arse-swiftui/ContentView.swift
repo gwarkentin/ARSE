c3b7820907b7f75e3ea10f82cadf1c73f6cc716c
Gabriel Warkentin <gabrielwarkentin@csu.fullerton.edu>
2023-03-19 22:08:11 -0700

    Update to pull and display data from Strapi Api

--- arse-swiftui/arse-swiftui/ContentView.swift
+++ arse-swiftui/arse-swiftui/ContentView.swift
@@ -38,25 +38,88 @@
 
 // yeah just leave the stuff above ig
 
+let API_KEY = "05f1aaa4009ce4e3d030f68a16dd74bf7562bca1014a0e26f05bd5eba8ed85feaf65ef0080df58d9cc54cbaf57f273a69808e10d523b61ee423d9499f3ecf162c14102ec8cf017f631a8cbd63055e2e607794770b23fb6238aad09290526686e183522282abafb9e8ade2383f29b29b5674f6b242c474eccf34a3cde434a1b86"
 
 import SwiftUI
 
-struct SinglePokeman: Hashable, Codable {
+struct ProductBrand: Hashable, Codable {
     let name: String
-    let weight: Int
+    let hero_blurb: String
 }
 
-struct Pokemon: Hashable, Codable {
+struct ProductBrandContainer: Hashable, Codable {
+    let id: Int
+    let attributes: ProductBrand
+}
+
+struct ProductBrandData: Hashable, Codable {
+    let data: ProductBrandContainer
+}
+
+struct ProductGroup: Hashable, Codable {
     let name: String
+}
+
+struct ProductGroupContainer: Hashable, Codable {
+    let id: Int
+    let attributes: ProductGroup
+}
+
+struct ProductGroupData: Hashable, Codable {
+    let data: ProductGroupContainer
+}
+
+struct ProductFeature: Hashable, Codable {
+    let name: String
+    let description: String
+    let main_body: String
+}
+
+struct ProductFeatureContainer: Hashable, Codable {
+    let id: Int
+    let attributes: ProductFeature
+}
+
+struct ProductFeaturesData: Hashable, Codable {
+    let data: [ProductFeatureContainer]
+}
+
+struct ProductDimension: Hashable, Codable {
+    let id: Int
+    let depth: Double
+    let height: Double
+    let width: Double
+    let weight: Double
+    let unit_length: String
+    let unit_weight: String
+}
+
+struct ProductImage: Hashable, Codable {
     let url: String
 }
 
-struct PokemonListResponse: Codable {
-    let results: [Pokemon]
+struct ProductAttributes: Hashable, Codable {
+    let sku: String
+    let description: String
+    let long_description: String
+    let brand: ProductBrandData
+    let group: ProductGroupData
+    let features: ProductFeaturesData
+//    let images: [ProductImage]
 }
 
+struct Product: Hashable, Codable {
+    let id: Int
+    let attributes: ProductAttributes
+}
+
+struct ProductListData: Codable {
+    let data: [Product]
+}
+
+
 class FetchModel: ObservableObject {
-    @Published var pokemon = [Pokemon]()
+    @Published var products = [Product]()
     
     var urlLink: String
     
@@ -68,33 +131,62 @@ class FetchModel: ObservableObject {
         guard let url = URL(string: urlLink) else {
             return
         }
+        var request = URLRequest(url: url)
+        request.addValue("application/json",forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(API_KEY)", forHTTPHeaderField: "Authorization")
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(PokemonListResponse.self, from: data) {
+                do {
+                    print("Data: ")
+                    print(data)
+                    let decodedResponse = try JSONDecoder().decode(ProductListData.self, from: data)
                     DispatchQueue.main.async {
-                        self.pokemon = decodedResponse.results
+                        self.products = decodedResponse.data
                     }
                     return
+                } catch {
+                    print(error)
                 }
             }
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
         }.resume()
     }
 }
 
+
 struct ContentView: View {
-    @StateObject var fetchModel = FetchModel(urlLink: "https://pokeapi.co/api/v2/pokemon?limit=100")
+    @StateObject var fetchModel = FetchModel(urlLink:
+        "http://localhost:1337/api/products/?populate=*&pagination[page]=1&pagination[pageSize]=10"
+    )
     
     var body: some View {
         NavigationView {
-            List(fetchModel.pokemon, id: \.self) { pokemon in
+            List(fetchModel.products, id: \.self) { product in
                 HStack{
-                    Text(pokemon.name)
-                        .font(.headline)
-                    NavigationLink(destination: Text(pokemon.name), label: {Text("")})
+                    HStack{
+                        Text(String(product.attributes.sku))
+                            .font(.headline)
+                        Text(String(product.attributes.description))
+                    }
+                    NavigationLink(
+                        destination: VStack{
+                            HStack{
+                                Text(String(product.attributes.brand.data.attributes.name))
+                                Text(product.attributes.sku)
+                            }
+                            HStack{
+                                Text("Group: ")
+                                Text(product.attributes.group.data.attributes.name)
+                            }
+                            //replace with "For feature in features"
+                            HStack{
+                                Text("Feature: ")
+                                Text(product.attributes.features.data[0].attributes.name)
+                            }
+                        }, label: {Text("Label")})
                 }
             }
-            .navigationBarTitle("Pokedex")
+            .navigationBarTitle("Products")
             .onAppear {
                 fetchModel.fetchData()
             }
